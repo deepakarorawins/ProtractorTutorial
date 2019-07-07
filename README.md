ea7123f79cc7dfeb7ee6d5717c0ce13a134d350f
Deepak Arora <deepak.arora@milwaukeetool.com>
2019-07-07 18:52:08 +0530

    Updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ProtractorTutorial
 Protractor Tutorial
+
+To execute the test from command line
+Open command line and navigate to project folder.
+execute following command: - 
+npm run protractor
+
+or 
+
+npm run smokesuite
+
+or
+
+npm run regressionsuite
+
+
+On new machine you may need to execute below command before above: - 
+
+npm install
+
+and
+
+npm run start
+ 
--- configuration.js
+++ configuration.js
@@ -2,11 +2,11 @@ var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 exports.config = {
 	// webdriver-manager start // to start selenium server
-	//seleniumAddress : 'http://localhost:4444/wd/hub', // we can start our test
-														// without starting
-														// selenium servr but we
-														// need to comment this
-														// line
+	// seleniumAddress : 'http://localhost:4444/wd/hub',
+	/*
+	 * we can start our test without starting selenium server but we need to
+	 * comment this line
+	 */
 	specs : [ 'ElementBasicsDataDriven.js' ],
 
 	// capabilities: {
@@ -20,13 +20,14 @@ exports.config = {
 			savePath : 'target/screenshots'
 		}))
 	},
-	
-	suites: {
-		Smoke: ['Alerts.js', 'Dropdowns.js'],
-		Regression: 'ElementBasicsDataDriven.js' // for single no need to provide brackets.
+
+	suites : {
+		Smoke : [ 'Alerts.js', 'Dropdowns.js' ],
+		Regression : 'ElementBasicsDataDriven.js' // for single no need to
+													// provide brackets.
 	},
-	
-	jasmineNodeOpts: {
-		showColors: true, // Use colors in command line report.
+
+	jasmineNodeOpts : {
+		showColors : true, // Use colors in command line report.
 	}
 };
